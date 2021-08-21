e364af9c2636646b3a3d752b69748be66542c0b6
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-08-21 11:42:56 +0530

    create header.blade.php

---
+++ resources/views/partials/guest/header.blade.php
@@ -0,0 +1,90 @@
+
+
+    <!-- Header -->
+    <header id="page-header" class="invisible" data-toggle="appear" data-class="animated fadeInDown">
+        <!-- Header Content -->
+        <div class="content-header">
+            <!-- Left Section -->
+            <div class="content-header-section">
+                <!-- Logo -->
+                <div class="content-header-item">
+                    <a class="link-effect font-w700 mr-5" href="/">
+                        <i class="si si-fire text-primary"></i>
+                        <span class="font-size-h4 text-dual-primary-dark">code</span><span class="font-size-h4 text-primary">base</span>
+                    </a>
+                </div>
+                <!-- END Logo -->
+            </div>
+            <!-- END Left Section -->
+
+            <!-- Right Section -->
+            <div class="content-header-section">
+                @if (Route::has('register'))
+                <a class="btn btn-sm btn-hero btn-noborder btn-alt-primary px-20" href="{{ route('login') }}">
+                    <i class="si si-rocket"></i> <span class="ml-5 d-none d-sm-inline-block">LOGIN</span>
+                </a>
+                @endif
+                    @if (Route::has('login'))
+                <a class="btn btn-sm btn-hero btn-noborder btn-alt-success px-20" href="{{ route('register') }}">
+                    <i class="si si-bag"></i> <span class="ml-5 d-none d-sm-inline-block">REGISTER</span>
+                </a>
+                    @endif
+            </div>
+            <!-- END Right Section -->
+        </div>
+        <!-- END Header Content -->
+    </header>
+
+
+
+
+{{--<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>--}}
