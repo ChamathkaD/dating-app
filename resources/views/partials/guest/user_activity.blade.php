3bb2e8d6c92a6eafbc55cd9846be3d4f3235f361
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-08-22 14:58:58 +0530

    create user_activity.blade.php

---
+++ resources/views/partials/guest/user_activity.blade.php
@@ -0,0 +1,91 @@
+<div class="bg-white">
+    <div class="content content-full">
+        <div class="pt-100 pb-50">
+            <div class="position-relative">
+                {{--<span class="text-back">05</span>--}}
+                <h2 class="font-w700 text-center mb-10">
+                    User <span class="text-primary"> Activity </span>
+                </h2>
+                <h3 class="h4 font-w400 text-muted text-center mb-50">
+                    Make your life easier and work more efficiently.
+                </h3>
+            </div>
+            <div class="row nice-copy my-10">
+                <div class="col-md-2 py-20">
+                    <div class="text-center">
+                        <img class="img-avatar img-avatar-thumb" src="assets/media/avatars/avatar9.jpg" alt="">
+                        <div class="font-w600 text-dark mt-10">Chama Dissanyaka</div>
+                        <div class="font-size-sm text-dark-op">2 seconds ago</div>
+                    </div>
+                </div>
+                <div class="col-md-2 py-20">
+                    <div class="text-center">
+                        <img class="img-avatar img-avatar-thumb" src="assets/media/avatars/avatar9.jpg" alt="">
+                        <div class="font-w600 text-dark mt-10">Chama Dissanyaka</div>
+                        <div class="font-size-sm text-dark-op">2 seconds ago</div>
+                    </div>
+                </div>
+                <div class="col-md-2 py-20">
+                    <div class="text-center">
+                        <img class="img-avatar img-avatar-thumb" src="assets/media/avatars/avatar9.jpg" alt="">
+                        <div class="font-w600 text-dark mt-10">Chama Dissanyaka</div>
+                        <div class="font-size-sm text-dark-op">2 seconds ago</div>
+                    </div>
+                </div>
+                <div class="col-md-2 py-20">
+                    <div class="text-center">
+                        <img class="img-avatar img-avatar-thumb" src="assets/media/avatars/avatar9.jpg" alt="">
+                        <div class="font-w600 text-dark mt-10">Chama Dissanyaka</div>
+                        <div class="font-size-sm text-dark-op">2 seconds ago</div>
+                    </div>
+                </div>
+                <div class="col-md-2 py-20">
+                    <div class="text-center">
+                        <img class="img-avatar img-avatar-thumb" src="assets/media/avatars/avatar9.jpg" alt="">
+                        <div class="font-w600 text-dark mt-10">Chama Dissanyaka</div>
+                        <div class="font-size-sm text-dark-op">2 seconds ago</div>
+                    </div>
+                </div>
+                <div class="col-md-2 py-20">
+                    <div class="text-center">
+                        <img class="img-avatar img-avatar-thumb" src="assets/media/avatars/avatar9.jpg" alt="">
+                        <div class="font-w600 text-dark mt-10">Chama Dissanyaka</div>
+                        <div class="font-size-sm text-dark-op">2 seconds ago</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="text-center">
+                <a class="btn btn-hero btn-noborder btn-alt-secondary min-width-175" href="be_pages_generic_todo.html">
+                    <i class="si si-rocket mr-5"></i> Online Listing
+                </a>
+            </div>
+
+          {{-- <div class="row nice-copy my-10">
+                <div class="col-md-4 py-20">
+                    <h4 class="font-size-xl font-w700 text-uppercase mb-10">
+                        <i class="fa fa-cogs text-elegance mr-5"></i> Preprocessor
+                    </h4>
+                    <p class="mb-0">Codebase CSS framework was built with Sass following a completely modular approach. It is easy to understand, customize and extend to fulfill your needs or project’s requirements.</p>
+                </div>
+                <div class="col-md-4 py-20">
+                    <h4 class="font-size-xl font-w700 text-uppercase mb-10">
+                        <i class="fa fa-terminal text-info mr-5"></i> Live Tasks
+                    </h4>
+                    <p class="mb-0">Smart tasks will serve your project, auto refresh on file changes, live compile your ES6 to ES5 and Sass to CSS while you work (also auto adding any required browser prefixes).</p>
+                </div>
+                <div class="col-md-4 py-20">
+                    <h4 class="font-size-xl font-w700 text-uppercase mb-10">
+                        <i class="fa fa-dot-circle-o text-danger mr-5"></i> Build
+                    </h4>
+                    <p class="mb-0">Time-saving task that will build a custom, minimized and production ready Codebase framework for you. Create your own variation based on your project.</p>
+                </div>
+            </div>--}}
+        </div>
+       {{-- <div class="row no-gutters justify-content-center pull-b text-center overflow-hidden">
+            <div class="col-sm-6 invisible" data-toggle="appear" data-class="animated zoomIn">
+                <img class="img-fluid" src="assets/media/various/landing-promo-smart-tools.png" srcset="assets/media/various/landing-promo-smart-tools@2x.png 2x" alt="Smart Tools Promo">
+            </div>
+        </div>--}}
+    </div>
+</div>
