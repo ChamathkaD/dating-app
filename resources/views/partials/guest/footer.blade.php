dafdd7c3e35381a79a0b88c23cd66473fd95cf3b
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-08-22 14:58:09 +0530

    create footer.blade.php

---
+++ resources/views/partials/guest/footer.blade.php
@@ -0,0 +1,13 @@
+<div class="bg-white">
+    <div class="content content-full text-center overflow-hidden">
+        <div class="py-100">
+            <h2 class="font-w700 mb-10">
+                Crafted with <i class="fa fa-heart text-danger"></i> by <a class="link-effect" href="https://1.envato.market/ydb">pixelcave</a>
+            </h2>
+            <h3 class="h4 font-w400 text-muted mb-50">Passionate web design and development with over 12.000 customers worldwide.</h3>
+            <a class="btn btn-hero btn-lg btn-noborder btn-alt-success mb-10 invisible" data-toggle="appear" data-class="animated zoomIn" href="https://1.envato.market/95j">
+                <i class="fa fa-shopping-bag mr-10"></i> Purchase Codebase
+            </a>
+        </div>
+    </div>
+</div>
