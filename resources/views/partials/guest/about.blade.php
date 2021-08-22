2002c1ecf07c4c5ace65c9659806fff88d42a35f
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-08-22 12:04:15 +0530

    create about.blade.php

---
+++ resources/views/partials/guest/about.blade.php
@@ -0,0 +1,28 @@
+<!-- Hero -->
+<div class="bg-white bg-pattern" style="background-image: url('{{ asset('assets/media/various/bg-pattern-inverse.png') }}');">
+    <div class="d-flex align-items-center">
+        <div class="content content-full">
+            <div class="row no-gutters w-100 py-100 overflow-hidden">
+                <div class="col-md-5 py-30 d-flex align-items-center invisible" data-toggle="appear">
+                    <div class="text-center text-md-left">
+                        <span class="d-inline-block bg-body-light rounded-pill py-5 px-15 mb-15 font-w600">Version 3.3</span>
+                        <h1 class="font-w600 font-size-h2 mb-20">
+                            MEET HIM HERE
+                        </h1>
+                        <p class="font-size-lg nice-copy text-muted mb-30">
+                            And among the signs of His power is that He created for you wives of your own kind, that you may incline and feel at ease towards them, and He made between you love and affection.
+                            Surah An-Rum, verse 21
+                        </p>
+                        <a class="btn btn-hero btn-alt-secondary" href="#">
+                            <i class="fa fa-arrow-right text-muted mr-5"></i> Introduction
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-7 py-30 d-none d-md-flex align-items-md-center justify-content-md-end invisible" data-toggle="appear" data-class="animated fadeInRight">
+                    <img class="img-fluid" src="assets/media/various/landing-promo-hero.png" srcset="assets/media/various/landing-promo-hero@2x.png 2x" alt="Hero Promo">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END Hero -->
