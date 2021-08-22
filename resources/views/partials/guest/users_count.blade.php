b6fb3e0fb6f13d60412f3b66779e57853b01600b
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-08-22 14:58:38 +0530

    create users_count.blade.php

---
+++ resources/views/partials/guest/users_count.blade.php
@@ -0,0 +1,39 @@
+<div class="col-md-12 py-30">
+{{-- <h3 class="h4 font-w700 text-uppercase text-center pb-10 border-b border-3x mb-0">Quick <span class="text-primary">Statistics</span>.</h3>--}}
+
+<!-- CountTo ([data-toggle="countTo"] is initialized in Helpers.coreAppearCountTo()) -->
+    <!-- For more info and examples you can check out https://github.com/mhuggins/jquery-countTo -->
+    <div class="row text-center">
+        <div class="col-sm-6 col-md-3 py-30">
+            <div class="mb-20">
+                <i class="si si-briefcase fa-3x text-primary"></i>
+            </div>
+            <div class="font-size-h1 font-w700 text-black mb-5" data-toggle="countTo" data-to="9600" data-after="+">0</div>
+            <div class="font-w600 text-muted text-uppercase">Active users
+
+
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-3 py-30">
+            <div class="mb-20">
+                <i class="si si-users fa-3x text-primary"></i>
+            </div>
+            <div class="font-size-h1 font-w700 text-black mb-5" data-toggle="countTo" data-to="760" data-after="+">0</div>
+            <div class="font-w600 text-muted text-uppercase">Singles</div>
+        </div>
+        <div class="col-sm-6 col-md-3 py-30">
+            <div class="mb-20">
+                <i class="si si-clock fa-3x text-primary"></i>
+            </div>
+            <div class="font-size-h1 font-w700 text-black mb-5" data-toggle="countTo" data-to="88600" data-after="+">0</div>
+            <div class="font-w600 text-muted text-uppercase">widow</div>
+        </div>
+        <div class="col-sm-6 col-md-3 py-30">
+            <div class="mb-20">
+                <i class="si si-badge fa-3x text-primary"></i>
+            </div>
+            <div class="font-size-h1 font-w700 text-black mb-5" data-toggle="countTo" data-to="60" data-after="+">0</div>
+            <div class="font-w600 text-muted text-uppercase">Doubt</div>
+        </div>
+    </div>
+</div>
