9c23149d7b6e2881441ea25e04ad623c75c5d1f6
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-08-22 14:58:25 +0530

    create here_bigin_a_love.blade.php

---
+++ resources/views/partials/guest/here_bigin_a_love.blade.php
@@ -0,0 +1,21 @@
+<div class="bg-body-light">
+    <div class="content content-full">
+        <div class="pt-100 pb-50">
+            <div class="position-relative">
+              {{--  <span class="text-back">02</span>--}}
+                <h2 class="font-w700 text-center mb-10">
+                    Here begins<span class="text-primary"> a love </span>
+                </h2>
+                <h3 class="h4 font-w400 text-muted text-center mb-50">
+                    Now, you are ready to get married and are looking for him. <br>
+                    Join myJodoh .. maybe he is here ..
+                </h3>
+            </div>
+            <div class="text-center">
+                <a class="btn btn-hero btn-noborder btn-alt-secondary min-width-175" href="be_pages_generic_todo.html">
+                    <i class="si si-rocket mr-5"></i> Sign Up Now
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
