44afc43edc6adcdcd044567f0a8c765c559bdd60
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-08-29 19:47:36 +0530

    home.blade.php

--- resources/views/partials/guest/user_activity.blade.php
+++ resources/views/partials/guest/user_activity.blade.php
@@ -61,31 +61,7 @@
                 </a>
             </div>
 
-          {{-- <div class="row nice-copy my-10">
-                <div class="col-md-4 py-20">
-                    <h4 class="font-size-xl font-w700 text-uppercase mb-10">
-                        <i class="fa fa-cogs text-elegance mr-5"></i> Preprocessor
-                    </h4>
-                    <p class="mb-0">Codebase CSS framework was built with Sass following a completely modular approach. It is easy to understand, customize and extend to fulfill your needs or project’s requirements.</p>
-                </div>
-                <div class="col-md-4 py-20">
-                    <h4 class="font-size-xl font-w700 text-uppercase mb-10">
-                        <i class="fa fa-terminal text-info mr-5"></i> Live Tasks
-                    </h4>
-                    <p class="mb-0">Smart tasks will serve your project, auto refresh on file changes, live compile your ES6 to ES5 and Sass to CSS while you work (also auto adding any required browser prefixes).</p>
-                </div>
-                <div class="col-md-4 py-20">
-                    <h4 class="font-size-xl font-w700 text-uppercase mb-10">
-                        <i class="fa fa-dot-circle-o text-danger mr-5"></i> Build
-                    </h4>
-                    <p class="mb-0">Time-saving task that will build a custom, minimized and production ready Codebase framework for you. Create your own variation based on your project.</p>
-                </div>
-            </div>--}}
         </div>
-       {{-- <div class="row no-gutters justify-content-center pull-b text-center overflow-hidden">
-            <div class="col-sm-6 invisible" data-toggle="appear" data-class="animated zoomIn">
-                <img class="img-fluid" src="assets/media/various/landing-promo-smart-tools.png" srcset="assets/media/various/landing-promo-smart-tools@2x.png 2x" alt="Smart Tools Promo">
-            </div>
-        </div>--}}
+
     </div>
 </div>
--- resources/views/partials/guest/users_count.blade.php
+++ resources/views/partials/guest/users_count.blade.php
@@ -1,9 +1,5 @@
 <div class="col-md-12 py-30">
-{{-- <h3 class="h4 font-w700 text-uppercase text-center pb-10 border-b border-3x mb-0">Quick <span class="text-primary">Statistics</span>.</h3>--}}
-
-<!-- CountTo ([data-toggle="countTo"] is initialized in Helpers.coreAppearCountTo()) -->
-    <!-- For more info and examples you can check out https://github.com/mhuggins/jquery-countTo -->
-    <div class="row text-center">
+       <div class="row text-center">
         <div class="col-sm-6 col-md-3 py-30">
             <div class="mb-20">
                 <i class="si si-briefcase fa-3x text-primary"></i>
