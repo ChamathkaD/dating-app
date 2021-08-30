4829f0d991a11f243ab27e2cdc0ab4b1f964520b
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-08-30 20:44:58 +0530

    app.blade.php updated

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,45 @@
+
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }} @yield('title')</title>
+
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+    <link rel="shortcut icon" href="{{ asset('assets/media/favicons/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/media/favicons/favicon-192x192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/media/favicons/apple-touch-icon-180x180.png') }}">
+    <!-- END Icons -->
+
+    <!-- Stylesheets -->
+
+    <!-- Fonts and Codebase framework -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700&display=swap">
+    <link rel="stylesheet" id="css-main" href="{{ asset('assets/css/codebase.min.css') }}">
+</head>
+<body>
+
+<div id="page-container" class="page-header-fixed page-header-glass main-content-boxed">
+    <div id="page-container" class="main-content-boxed">
+        <main id="main-container">
+            @yield('app-content')
+        </main>
+    </div>
+</div>
+
+<script src="{{asset('assets/js/codebase.core.min.js')}}"></script>
+<script src="{{asset('assets/js/codebase.app.min.js')}}"></script>
+
+<!-- Page JS Plugins -->
+<script src="{{asset('assets/js/plugins/jquery-validation/jquery.validate.min.js')}}"></script>
+<script src="{{asset('assets/js/pages/op_auth_signup.min.js')}}"></script>
+<script src="{{asset('assets/js/plugins/jquery-vide/jquery.vide.min.js')}}"></script>
+
+</body>
+</html>
+
