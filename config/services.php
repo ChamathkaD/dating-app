e580a10b837ec5c66f3595438cd0dbe92b6efb9f
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-09-02 12:06:31 +0530

    services.php updated

--- config/services.php
+++ config/services.php
@@ -33,13 +33,13 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'FACEBOOK_callback-url',
+        'redirect' => env('FACEBOOK_CALLBACK_URL'),
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'GOOGLE_callback-url',
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
     ],
 
 ];
